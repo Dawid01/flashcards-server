a4b0b673648133749174cc36d6df5172384de747
HardMobileGames <hardmobilegames@gmail.com>
2019-02-23 12:55:07 +0100

    init

---
+++ src/main/java/pl/sykisoft/flashcards/server/flashcardsserver/FlashcardsServerApplication.java
@@ -0,0 +1,13 @@
+package pl.sykisoft.flashcards.server.flashcardsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsServerApplication.class, args);
+	}
+
+}
